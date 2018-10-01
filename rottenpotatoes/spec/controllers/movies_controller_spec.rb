0c09d81d691a0aef90ae39ca7ef752be19b02000
EC2 Default User <ec2-user@ip-172-31-27-78.us-east-2.compute.internal>
2018-10-01 02:47:07 +0000

    add view for similar movies

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
+    it "should find movies by similar director if the movie has a director" do
+    end
+    
+    it "should not find any similar movie if the movie has no director" do
+    end 
 
 end
